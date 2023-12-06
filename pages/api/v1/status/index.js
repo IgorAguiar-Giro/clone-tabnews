0f5df5d62803653f00bf03740fcca59085b4b64b
Igor Aguiar <80079216+IgorAguiar-Giro@users.noreply.github.com>
2023-12-06 14:36:35 +0000

    Add more itens on the respose body and tests to `get.test.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,24 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+  const result = await database.query(
+    "SELECT SUBSTRING(version() FROM 'PostgreSQL ([0-9]+\\.[0-9]+)') AS postgres_version"
+  );
+  const version = result.rows[0].postgres_version;
+
+  const maxConnections = await database.query("SHOW MAX_CONNECTIONS");
+
+  const usedConnections = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity"
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    server_version: version,
+    max_connections: maxConnections.rows[0].max_connections,
+    used_connections: usedConnections.rows[0].count,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,18 @@
-
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt); //check if date is ISO8601
+
+  expect(Number(responseBody.server_version)).toBe(16); //hardcoded test version for Postgres Version
+
+  expect(Number(responseBody.max_connections)).toBe(100); //hardcoded test for max connections
+
+  expect(Number(responseBody.used_connections)).toBeLessThanOrEqual(
+    Number(responseBody.max_connections)
+  ); //Test for used connections
 });
