9f33d055f2f8d19242e33bd8d9d9f8a55ff32931
Igor Aguiar <80079216+IgorAguiar-Giro@users.noreply.github.com>
2025-09-17 19:51:20 +0000

    makes `npm test` more robust and `orchestrator.js`

---
+++ tests/orchestrator.js
@@ -0,0 +1,21 @@
+import retry from "async-retry";
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    // Wait for the web server to be ready
+    return retry(fetchStatusPage, {
+      retries: 100,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+    }
+  }
+}
+
+export default {
+  waitForAllServices,
+};
